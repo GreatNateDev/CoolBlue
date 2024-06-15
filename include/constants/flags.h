0d0a6518044ce203fd5db27f80b366e460f53870
Nate <99703235+GreatNateDev@users.noreply.github.com>
2024-06-15 14:21:18 -0500

    make some neat fixes

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1318,7 +1318,8 @@
 #define FLAG_0x500               0x500
 #define FLAG_0x501               0x501
 #define FLAG_0x502               0x502
-#define TRAINER_FLAGS_START      (FLAG_0x502 + 1)
+#define has_taken_kid_candy      0x503
+#define TRAINER_FLAGS_START      (FLAG_0x503 + 1)
 #define TRAINER_FLAGS_END        (TRAINER_FLAGS_START + MAX_TRAINERS_COUNT - 1) // 0x7FF
 
 // SYSTEM FLAGS
